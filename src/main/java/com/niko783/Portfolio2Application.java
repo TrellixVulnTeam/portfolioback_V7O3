00e872ed3418cfd13202dc73cf26b43155576894
Niko Lopez <pronikolopez@gmail.com>
2022-07-27 16:54:11 -0300

    nueva actualización

--- src/main/java/com/niko783/Portfolio2Application.java
+++ src/main/java/com/niko783/Portfolio2Application.java
@@ -1,13 +1,14 @@
 package com.niko783;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Portfolio2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Portfolio2Application.class, args);
+		System.out.println("Niko783 Backend");
+		//SpringApplication.run(Portfolio2Application.class, args);		
 	}
 
 }
