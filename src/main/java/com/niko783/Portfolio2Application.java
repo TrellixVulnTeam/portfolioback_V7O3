44263f8e9142aaac20c62cf6116c46524522c5ec
Niko Lopez <pronikolopez@gmail.com>
2022-07-28 13:17:47 -0300

    deploy

--- src/main/java/com/niko783/Portfolio2Application.java
+++ src/main/java/com/niko783/Portfolio2Application.java
@@ -1,6 +1,6 @@
 package com.niko783;
 
-//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -8,7 +8,7 @@ public class Portfolio2Application {
 
 	public static void main(String[] args) {
 		System.out.println("Niko783 Backend");
-		//SpringApplication.run(Portfolio2Application.class, args);		
+		SpringApplication.run(Portfolio2Application.class, args);		
 	}
 
 }
